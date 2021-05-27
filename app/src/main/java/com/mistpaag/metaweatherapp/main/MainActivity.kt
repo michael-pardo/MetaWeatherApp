50edecbdc877b8e88c7dbd6b816ecbf0eb493dcf
Michael Pardo <michael.pardo@unillanos.edu.co>
2021-05-27 11:10:38 -0500

    add nav host fragment in main activity

--- app/src/main/java/com/mistpaag/metaweatherapp/main/MainActivity.kt
+++ app/src/main/java/com/mistpaag/metaweatherapp/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.mistpaag.metaweatherapp.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mistpaag.metaweatherapp.R
+import com.mistpaag.metaweatherapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
