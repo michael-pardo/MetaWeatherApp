4f408f2d6db4ee4e665e8f448502138e2e429085
Michael Pardo <michael.pardo@unillanos.edu.co>
2021-05-27 18:49:14 -0500

    see location list when search - fragment

--- app/src/main/java/com/mistpaag/metaweatherapp/presentation/main/search/SearchLocationFragment.kt
+++ app/src/main/java/com/mistpaag/metaweatherapp/presentation/main/search/SearchLocationFragment.kt
@@ -1,10 +1,13 @@
 package com.mistpaag.metaweatherapp.presentation.main.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.mistpaag.metaweatherapp.databinding.SearchLocationFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,10 +34,37 @@ class SearchLocationFragment : Fragment() {
     }
 
     private fun setupUI(){
+        setupSearchBar()
         val adapter = SearchLocationAdapter{
 
         }
         binding.searchRecycler.adapter = adapter
+
+        viewModel.state.observe(viewLifecycleOwner,{ state ->
+            binding.progressBar.isVisible = state is SearchLocationState.Loading
+            when (state) {
+                is SearchLocationState.Error -> showMessage(state.message)
+                SearchLocationState.Loading -> {}
+                is SearchLocationState.SearchList -> adapter.submitList(state.list.toMutableList())
+            }
+        })
+    }
+
+    private fun setupSearchBar(){
+        with(binding.searchBarIncluded){
+            searchText.doOnTextChanged { text, _, _, _ ->
+                viewModel.setIntentEvent(
+                    SearchLocationIntent.SearchLocation(text.toString())
+                )
+            }
+            closeImage.setOnClickListener {
+                searchText.setText("")
+            }
+        }
+    }
+
+    private fun showMessage(message: String){
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
 
