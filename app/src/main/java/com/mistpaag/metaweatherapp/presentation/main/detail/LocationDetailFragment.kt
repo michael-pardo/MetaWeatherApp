ac54bfa59dc7672a4894c994eb64562ae4d640dc
Michael Pardo <michael.pardo@unillanos.edu.co>
2021-05-27 22:05:41 -0500

    add location detail fragment

---
+++ app/src/main/java/com/mistpaag/metaweatherapp/presentation/main/detail/LocationDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.mistpaag.metaweatherapp.presentation.main.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.mistpaag.metaweatherapp.R
+import com.mistpaag.metaweatherapp.databinding.LocationDetailFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LocationDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LocationDetailFragment()
+    }
+
+    private val viewModel by viewModels<LocationDetailViewModel>()
+    private lateinit var binding: LocationDetailFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = LocationDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/mistpaag/metaweatherapp/presentation/main/detail/LocationDetailViewModel.kt
@@ -0,0 +1,12 @@
+package com.mistpaag.metaweatherapp.presentation.main.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class LocationDetailViewModel @Inject constructor(
+
+): ViewModel() {
+    // TODO: Implement the ViewModel
+}
