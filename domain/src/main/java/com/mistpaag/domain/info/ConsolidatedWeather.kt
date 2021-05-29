e02f9821a0945b77b02c540753dbae4415d43b66
Michael Pardo <michael.pardo@unillanos.edu.co>
2021-05-28 23:58:30 -0500

    modify consolidated weather model

--- domain/src/main/java/com/mistpaag/domain/info/ConsolidatedWeather.kt
+++ domain/src/main/java/com/mistpaag/domain/info/ConsolidatedWeather.kt
@@ -3,6 +3,7 @@ package com.mistpaag.domain.info
 data class ConsolidatedWeather(
     val id: Long,
     val weatherStateName: String,
+    val applicableDate: String,
     val minTemp: Double,
     val maxTemp: Double,
     val weatherStateAbbr: String,
